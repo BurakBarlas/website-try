5cdfcdff02e692c7d94ff26356a98e786c431b77
Burak Barlas <55807664+BurakBarlas@users.noreply.github.com>
2020-08-16 00:59:07 +0300

    edited background

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p> Hello</p>
+        <a
+          className="Github"
+          href="https://github.com/BurakBarlas"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <i className="fab fa-github">Github</i> 
+        </a>
+        
+        <p className="deneme">hey</p>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
