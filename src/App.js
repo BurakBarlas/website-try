000dea4165acb2e6fa570fba50ebc47f80d76e1a
Burak Barlas <55807664+BurakBarlas@users.noreply.github.com>
2020-08-19 20:36:29 +0300

    added new images

--- src/App.js
+++ src/App.js
@@ -3,20 +3,56 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const title = "Burak Barlas";
+
   return (
+
     <div className="App">
+      
+    <h1>{title}</h1>
+    
       <header className="App-header">
-      <p> Hello</p>
-        <a
-          className="Github"
-          href="https://github.com/BurakBarlas"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <i className="fab fa-github">Github</i> 
-        </a>
+          <div className="ClickIcons" >
+            <a
+              className="Github"
+              href="https://github.com/BurakBarlas"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <i class="fab fa-github-square"></i>
+            </a>
+              
+            <a
+              className="linkedin"
+              href="https://www.linkedin.com/in/burak-barlas-337324184/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+
+            <a
+              className="Mail"
+              href="mailto:fpburak@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >  
+            <i class="fas fa-envelope-square"></i>
+            </a>
+
+            {/* <a
+             className="CV"
+             href="mail"
+             target="_blank"
+             rel="noopener noreferrer"
+            >
+            <i class="fas fa-file"></i>  
+            </a> */}
+
+          </div>
+
+          
         
-        <p className="deneme">hey</p>
       </header>
       
     </div>
