2d6f79ed9c8a8f342a0f7799a9b29a755ab9bc71
Burak Barlas <fpburak@gmail.com>
2021-04-11 00:00:25 +0300

    cv added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Components from './components/components';
 import BackGroundImage from './Assets/BackGroundImage.jpeg';
 import FunctionClick from './components/FunctionClick';
-import { BrowserRouter as Router, Link, Route } from  'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import ClassClick from './components/ClassClick';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -15,41 +15,41 @@ function App() {
   return (
     <Router>
       <div className="App">
-        
-      <div className="main-container"> 
 
-        <header className="App-header">
-          
-          <div className="page">
-            <h1>About Me</h1>
-          </div>
+        <div className="main-container">
 
-          <div className="menu">
-            <Link to={'/about'} >
-              <h2>about me</h2>
-            </Link>
-            <Link to={'/projects'}>
-            <h2>projects</h2>
-            </Link>
-          </div>
-          
-          {/* <FunctionClick/>
+          <header className="App-header">
+
+            <div className="page">
+              <h1>About Me</h1>
+            </div>
+
+            <div className="menu">
+              <Link to={'/about'} >
+                <h2>about me</h2>
+              </Link>
+              <Link to={'/projects'}>
+                <h2>projects</h2>
+              </Link>
+            </div>
+
+            {/* <FunctionClick/>
           <ClassClick /> */}
-          
-        </header>
 
-        <div className="container">
-        <Route path={['/about', "/"]} component={ About }/>
-        <Route path='/projects' component={ Projects }/>
+          </header>
+
+          <div className="container">
+            <Route path={['/about', "/"]} component={About} />
+            <Route path='/projects' component={Projects} />
 
             {/* <p>I am Burak barlas. I'm studying at Bahcesehir University in Istanbul. Yet, I'm beginning of my career. 
               I am interested in learning new computer language, trying to improve myself by making new projects.</p>
 
             <img className="background-image" src={BackGroundImage} alt="Bad Connection Huh!" /> */}
-        
-        </div>
 
-        <div className="ClickIcons" >
+          </div>
+
+          <div className="ClickIcons" >
 
             <a
               className="Github"
@@ -57,9 +57,9 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-            <i className="fab fa-github-square"></i>
+              <i className="fab fa-github-square"></i>
             </a>
-              
+
             <a
               className="linkedin"
               href="https://www.linkedin.com/in/burak-barlas-337324184/"
@@ -74,25 +74,25 @@ function App() {
               href="mailto:fpburak@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
-            >  
-            <i className="fas fa-envelope-square"></i>
+            >
+              <i className="fas fa-envelope-square"></i>
             </a>
 
-            {/* <a
-             className="CV"
-             href="mail"
-             target="_blank"
-             rel="noopener noreferrer"
+            <a
+              className="CV"
+              href="mail"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-            <i class="fas fa-file"></i>  
-            </a> */}
+              <i class="fas fa-file-alt"></i>
+            </a>
+          </div>
+
         </div>
-          
+
+        <div className="backGroundLayer"></div>
+
       </div>
-      
-      <div className="backGroundLayer"></div>
-        
-    </div>
     </Router>
   );
 }
